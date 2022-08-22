b370a5854a595657d50af404d55cb4fc173a8dc6
bian-hengwei <55875341+bian-hengwei@users.noreply.github.com>
2022-08-23 03:36:36 +0800

    Initial repo setup

---
+++ src/main/java/edu/ntu/mdpg7algo/Test.java
@@ -0,0 +1,7 @@
+package edu.ntu.mdpg7algo;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello MDP!");
+    }
+}
---
+++ src/main/java/edu/ntu/mdpg7algo/arena/Arena.java
@@ -0,0 +1,9 @@
+package edu.ntu.mdpg7algo.arena;
+
+import lombok.Data;
+
+/**
+ * @author Bian Hengwei
+ */
+@Data
+public class Arena {}
