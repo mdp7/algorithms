dbaf0e98eba8af46cdd301d6c0c0d9a5bafeb4b1
Andre Lim <Andre_lim99@outlook.com>
2022-08-27 13:33:39 +0800

    Added several public static methods for calculations such as angle, rotation. Added a distance calculator given a vector as well

--- src/main/java/edu/ntu/mdpg7algo/models/Vector.java
+++ src/main/java/edu/ntu/mdpg7algo/models/Vector.java
@@ -4,11 +4,38 @@ import lombok.Data;
 
 @Data
 public class Vector {
+    public static Vector vectorRotateCounterClockwise(Vector v1, double angle){
+        double _x = v1.getX() * Math.cos(angle) - v1.getY() * Math.sin(angle);
+        double _y = v1.getX() * Math.sin(angle) + v1.getY() * Math.cos(angle);
+
+        return new Vector(_x, _y);
+    }
+    public static double calculateAngle(Vector V1, Vector V2, boolean isRightTurn){
+        double angle = Math.atan2(V2.getY(), V2.getX()) - Math.atan2(V1.getY(), V1.getX());
+        if(isRightTurn && angle > 0){
+            angle -= 2 * Math.PI;
+        } else if (!isRightTurn && angle < 0) {
+            angle += 2 * Math.PI;
+        }
+        return angle;
+    }
+
     private double x;
     private double y;
+
+    public Vector(Vector from, Vector to){
+        this.x = to.getX() - from.getX();
+        this.y = to.getY() - from.getY();
+    }
 
     public Vector(double x, double y) {
         this.x = x;
         this.y = y;
+    }
+
+    public double getLength(){
+        double distance = Math.hypot(Math.abs(this.x), Math.abs(this.y));
+        return distance;
+
     }
 }
