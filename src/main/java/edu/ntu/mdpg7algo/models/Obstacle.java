aaf6df76b6e6d61edffe4b10f493b8706e83a5f3
Andre Lim <Andre_lim99@outlook.com>
2022-08-27 09:39:11 +0800

    Added compute where robot shd be to detect object image

--- src/main/java/edu/ntu/mdpg7algo/models/Obstacle.java
+++ src/main/java/edu/ntu/mdpg7algo/models/Obstacle.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class Obstacle {
+    private final double robotOffset = 8;
     
     private Position position;
     private Dir dir;
@@ -21,7 +22,18 @@ public class Obstacle {
         this.dir = dir;
         this.detected = detected;
     }
-    
+
+
+    public Position computeRobotPosition(){
+        return switch (this.dir){
+            case UP -> new Position(position.getX(), position.getY() + robotOffset, dir.DOWN.theta);
+            case DOWN -> new Position(position.getX(), position.getY() - robotOffset, dir.UP.theta);
+            case LEFT -> new Position(position.getX()  - robotOffset, position.getY(), dir.RIGHT.theta);
+            case RIGHT -> new Position(position.getX()  + robotOffset, position.getY(), dir.LEFT.theta);
+        };
+    }
+
+
     @Override
     public String toString() {
         return ("x: " + this.position.getX() + " y: " + this.position.getY() + " dir: " + this.dir);
