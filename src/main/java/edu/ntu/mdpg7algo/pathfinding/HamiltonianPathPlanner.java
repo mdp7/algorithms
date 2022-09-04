f5e528450dbea36b88f542d130635f5816c8767e
Andre Lim <Andre_lim99@outlook.com>
2022-09-04 15:55:59 +0800

    Hamiltonian path finder and testing

--- src/main/java/edu/ntu/mdpg7algo/pathfinding/HamiltonianPathPlanner.java
+++ src/main/java/edu/ntu/mdpg7algo/pathfinding/HamiltonianPathPlanner.java
@@ -1,2 +1,77 @@
-package edu.ntu.mdpg7algo.pathfinding;public class HamiltonianPathPlanner {
+package edu.ntu.mdpg7algo.pathfinding;
+
+import edu.ntu.mdpg7algo.models.Arena;
+import edu.ntu.mdpg7algo.models.Obstacle;
+import lombok.Data;
+
+import java.util.ArrayList;
+
+@Data
+public class HamiltonianPathPlanner {
+
+    private Arena arena;
+    private double minCost = 10000;
+    private int[] order;
+
+    private int iterations = 0;
+
+
+
+    public HamiltonianPathPlanner(Arena arena) {
+        this.arena = arena;
+    }
+
+
+    private void travellingSalesMan(double[][] dist, boolean[] visited, int cur, int numGoals, int count, double cost, int[] order){
+
+//        If all goals reached
+        if(count == numGoals){
+//            if cost of path < current min
+            if(cost < minCost) {
+                minCost = cost;
+                this.order = order;
+            }
+        }
+
+//        Go to neighbor node
+        for(int i = 0; i < numGoals; i++){
+            this.iterations++;
+            if(!visited[i]){
+//                Cost to travel to next node
+                double newCost = cost + dist[cur][i];
+                if(newCost > minCost) continue;
+
+                visited[i] = true;
+//                Set order
+                int[] newOrder = order.clone();
+                newOrder[count] = i;
+
+
+                travellingSalesMan(dist, visited, i, numGoals, count + 1, newCost, newOrder);
+                visited[i] = false;
+            }
+        }
+
+    }
+
+
+    /*
+    dist is a 2D array for distance between every obstacles/start
+    numGoals = # of obstacles and Start
+     */
+
+    public int[] planPath(double[][] dist, int numGoals){
+        boolean[] visited = new boolean[numGoals];
+        int[] order = new int[numGoals];
+
+
+        for(int i = 0; i < visited.length; i++){
+            visited[i] = false;
+            order[i] = 0;
+        }
+//      mark start as true
+        visited[0] = true;
+        travellingSalesMan(dist, visited, 0, numGoals, 1, 0, order);
+        return this.order;
+    }
 }
--- src/main/java/edu/ntu/mdpg7algo/pathfinding/PathTesting.java
+++ src/main/java/edu/ntu/mdpg7algo/pathfinding/PathTesting.java
@@ -1,2 +1,65 @@
-package edu.ntu.mdpg7algo.pathfinding;public class PathTesting {
+package edu.ntu.mdpg7algo.pathfinding;
+
+import edu.ntu.mdpg7algo.models.Arena;
+import edu.ntu.mdpg7algo.models.Obstacle;
+
+import java.util.ArrayList;
+
+public class PathTesting {
+    public static void main(String[] args) {
+
+        ArrayList<Obstacle> obstacles = new ArrayList<>();
+        //      fake start obstacle
+        obstacles.add(new Obstacle(0,0,Obstacle.Facing.UP, Obstacle.ObstacleImage.E));
+
+//        Actual Obstacles
+        obstacles.add(new Obstacle(9, 13, Obstacle.Facing.UP, Obstacle.ObstacleImage.A));
+        obstacles.add(new Obstacle(5, 5, Obstacle.Facing.LEFT, Obstacle.ObstacleImage.B));
+        obstacles.add(new Obstacle(5, 10, Obstacle.Facing.RIGHT, Obstacle.ObstacleImage.C));
+        obstacles.add(new Obstacle(1, 1, Obstacle.Facing.DOWN, Obstacle.ObstacleImage.F));
+
+
+        Arena arena = new Arena();
+        arena.setObstacles(obstacles);
+        /*
+        set start point
+        Create distance 2D array
+         */
+        double [][] dist = new double[obstacles.size()][obstacles.size()];
+
+        for(int i = 0; i < obstacles.size(); i++){
+            for (int j = 0; j < obstacles.size(); j++){
+                if(i == j) dist[i][j] = Double.MAX_VALUE;
+
+                else {
+                    dist[i][j] = computeDistance(obstacles.get(i), obstacles.get(j));
+                }
+            }
+        }
+
+        for(int i = 0; i < obstacles.size(); i++){
+            for (int j = 0; j < obstacles.size(); j++){
+                System.out.print(dist[i][j] + "\t");
+                }
+            System.out.println();
+            }
+
+
+
+        HamiltonianPathPlanner h = new HamiltonianPathPlanner(arena);
+        int [] order = h.planPath(dist, obstacles.size());
+        System.out.println("Number of iterations: "+h.getIterations() + "\nMin cost: " + h.getMinCost());
+        for(int i = 0; i < order.length; i++){
+            System.out.println(order[i] + "\t");
+
+        }
+
+
+    }
+
+    public static double computeDistance(Obstacle o1, Obstacle o2){
+        double delX = o1.getX() - o2.getX();
+        double delY = o1.getY() - o2.getY();
+        return Math.hypot(delX, delY);
+    }
 }
