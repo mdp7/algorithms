41d7bccffbd02d0cdc4013e456235b34572728f2
Andre Lim <Andre_Lim99@outlook.com>
2022-10-05 11:12:48 +0800

    Added custom start variables

--- python/algorithm/const.py
+++ python/algorithm/const.py
@@ -1,17 +1,18 @@
 import socket
 
+from algorithm.entities.assets.direction import Direction
+
 # Obstacle Layout
 OBSTACLE_LAYOUT = [
     [[55, 75, -90], [55, 135, 180], [125, 95, 0], [155, 45, 90], [155, 155, -90]],
-    [[105, 55, 0], [175, 85, 90], [165, 155, 180]],
+    [[15, 145, -90], [145, 105, 180]],
     [[15, 95, 90], [45, 125, -90], [75, 95, 0], [135, 115, 180], [115, 35, 90]],
     [[25, 185, -90], [125, 185, -90], [105, 25, 0], [15, 115, -90], [125, 85, 180]],
     [[25, 185, -90], [125, 185, -90], [105, 25, 180], [15, 115, -90], [125, 85, 180]],
     [[15, 125, -90], [45, 185, -90], [115, 185, -90], [175, 115, 180], [115, 45, 90]],
     [[165, 145, 90], [165, 65, -90], [45, 75, 0], [35, 145, 0]],
     [[65, 65, -90], [155, 75, 180], [155, 135, 180], [45, 165, 0]],#, [65, 115, 180]]
-    [[65, 165, -90], [175, 145, 180], [175, 95, 180], [95, 85, 90], [115, 175, -90], [145, 15, 0], [75, 15, 180], [105, 15, 90]],
-    [[55, 95, -90], [75, 145, 180], [125, 95, 0], [155, 155, -90], [155, 45, 180]]
+    [[65, 165, -90], [175, 145, 180], [175, 95, 180], [95, 85, 90], [115, 175, -90], [145, 15, 0], [75, 15, 180], [105, 15, 90]]
 ]
 
 # PyGame settings
@@ -32,6 +33,7 @@ ROBOT_LENGTH = 35 * SCALING_FACTOR
 ROBOT_TURN_RADIUS = 30 * SCALING_FACTOR
 ROBOT_LEFT_TURN_RADIUS = 20 * SCALING_FACTOR
 ROBOT_RIGHT_TURN_RADIUS = 30 * SCALING_FACTOR
+ROBOT_TPT_TURN_RADIUS = 20 * SCALING_FACTOR
 
 
 ROBOT_SPEED_PER_SECOND = 100 * SCALING_FACTOR
@@ -39,8 +41,9 @@ ROBOT_S_FACTOR = ROBOT_LENGTH / ROBOT_TURN_RADIUS  # Please read briefing notes 
 ROBOT_SAFETY_DISTANCE = 15 * SCALING_FACTOR
 ROBOT_SCAN_TIME = 0.25  # Time provided for scanning an obstacle image in seconds.
 
-ROBOT_CUSTOM_START_X = 105 * SCALING_FACTOR
-ROBOT_CUSTOM_START_Y = 25 * SCALING_FACTOR
+ROBOT_CUSTOM_START_X = 15
+ROBOT_CUSTOM_START_Y = 15
+ROBOT_CUSTOM_START_DIR = Direction.TOP
 
 
 # Grid Attributes
@@ -52,13 +55,13 @@ GRID_NUM_GRIDS = GRID_LENGTH // GRID_CELL_LENGTH
 # Obstacle Attributes
 OBSTACLE_LENGTH = 10 * SCALING_FACTOR
 # OBSTACLE_SAFETY_WIDTH = ROBOT_SAFETY_DISTANCE // 3 * 4  # With respect to the center of the obstacle
-OBSTACLE_SAFETY_WIDTH = 25 * SCALING_FACTOR
+OBSTACLE_SAFETY_WIDTH = 20 * SCALING_FACTOR
 OBSTACLE_TARGET_OFFSET = 0 * SCALING_FACTOR
 
 # Path Finding Attributes
 PATH_TURN_COST = 999 * ROBOT_SPEED_PER_SECOND * ROBOT_TURN_RADIUS
 
-TPT_MUL = 1.05
+TPT_MUL = 5
 
 # NOTE: Higher number == Lower Granularity == Faster Checking.
 # Must be an integer more than 0! Number higher than 3 not recommended.
